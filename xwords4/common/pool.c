e2bb9ebd11aedd1c1efd76e796d8920d18390737
franc walter <francwalter@gmx.net>
2026-03-18 10:22:51 +0100

    Drawn tiles to logcat

--- xwords4/common/pool.c
+++ xwords4/common/pool.c
@@ -2,14 +2,14 @@
  * Copyright 2000 by Eric House (xwords@eehouse.org).  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
@@ -147,6 +147,7 @@ pool_requestTiles( PoolContext* pool, Tile* tiles, XP_U16* maxNum )
 {
     XP_S16 numWanted = *maxNum;
     XP_U16 numWritten = 0;
+    Tile* base = tiles;
 
     XP_ASSERT( numWanted >= 0 );
 
@@ -163,6 +164,16 @@ pool_requestTiles( PoolContext* pool, Tile* tiles, XP_U16* maxNum )
         ++numWritten;
     }
     *maxNum = numWritten;
+
+    if ( numWritten > 0 ) {
+        char buf[128];
+        int offset = 0;
+        offset += XP_SNPRINTF( buf + offset, sizeof(buf) - offset, "Drawn %d tiles: ", numWritten );
+        for ( XP_U16 i = 0; i < numWritten; i++ ) {
+            offset += XP_SNPRINTF( buf + offset, sizeof(buf) - offset, "%d ", base[i] );
+        }
+        XP_LOGFF( "%s", buf );
+    }
 
 #ifdef BLANKS_FIRST
     pool->lettersLeft[pool->blankIndex] = oldCount - 1;
@@ -296,4 +307,3 @@ pool_dumpSelf( const PoolContext* pool )
              pool->numTilesLeft,  buf );
 }
 #endif
-
